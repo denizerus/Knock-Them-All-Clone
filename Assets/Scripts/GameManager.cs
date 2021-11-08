3db4673894a60025a94147f2ec90a1181912f5c7
denizerk <denizerkeskiner@hotmail.com>
2021-11-08 20:20:55 +0300

    Revert "Revert "Drag & Drop Mechanic İmplemented""
    This reverts commit 0cb76d7179c4f1c7261ebe9e29f9232646454aac.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform BallSpawnPoint;
+    public GameObject Ball;
+
+    void Start()
+    {
+        Instantiate(Ball, BallSpawnPoint);
+    }
+    
+    void Update()
+    {
+
+    }
+
+}
