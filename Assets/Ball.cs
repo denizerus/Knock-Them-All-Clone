b3c3af9d603509eb86457de48cdd869e12e42969
denizerk <denizerkeskiner@hotmail.com>
2021-11-16 20:24:42 +0300

    Explosion of enemy is developed

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -6,9 +6,32 @@ public class Ball : MonoBehaviour
 {
     public bool isUsable;
 
-    void Start()
+    public float expForce, radius;
+
+    private void Start()
     {
+        expForce = 100f;
+        radius = 2f;
         isUsable = true;
     }
 
+
+    private void OnCollisionEnter(Collision other)
+    {
+        KnockBack();
+    }
+
+    void KnockBack()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearby in colliders)
+        {
+            Rigidbody rig = nearby.GetComponent<Rigidbody>();
+            if (rig != null &&  nearby.transform.tag != "Ball" )
+            {
+                rig.AddExplosionForce(expForce, transform.position, radius);
+            }
+        }
+    }
 }
--- Assets/DetectEnemyPoint.cs
+++ Assets/DetectEnemyPoint.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class DetectEnemyPoint : MonoBehaviour
 {
+    Rigidbody rig;
 
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Enemy")
         {
-            Debug.Log("GOOOOOOOOOOLLLL");
             gameObject.GetComponent<BoxCollider>().enabled = false;
             GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>().numOfEnemy--;
+
+
+
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ball")
+        {
+            rig = gameObject.transform.parent.gameObject.transform.Find("EnemyModel").GetComponent<Rigidbody>();
+            rig.AddForce(new Vector3(0, 500f, 0));
         }
     }
+
 }
